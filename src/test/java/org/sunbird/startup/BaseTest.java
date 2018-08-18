b9103750622c07c03b91c692b80437f5990cfa01
AbhinavQualitrix <abhinav.kumar@qualitrix.com>
2018-08-18 08:26:08 -0700

    basetest wthout haedless

--- src/test/java/org/sunbird/startup/BaseTest.java
+++ src/test/java/org/sunbird/startup/BaseTest.java
@@ -4,10 +4,6 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 
@@ -20,38 +16,11 @@ public abstract class BaseTest implements IAutoConst {
 
 	}
 
-	@SuppressWarnings("deprecation")
 	@BeforeMethod
 	public void openApplication() throws IOException, InterruptedException 
 	{
-		/*DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
-		capabilities.setCapability("chrome.binary", "<Path to binary>");
-		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-		capabilities.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-		capabilities.setCapability("applicationCacheEnabled", "true");
-		capabilities.setCapability("browserConnectionEnabled", "true");
-		capabilities.setCapability("databaseEnabled", "true");
-		capabilities.setCapability("networkConnectionEnabled", "true");
-		driver = new ChromeDriver(capabilities); */
-		
-		//ChromeOptions options = new ChromeOptions();
-		//options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-  		//options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-  		//options.addArguments("--headless");
-		//options.addArguments("start-fullscreen");
-		//, "window-size=1024,768", "--no-sandbox");
-  		
-  		/*options.setAcceptInsecureCerts(true);
-  		options.setCapability("applicationCacheEnabled", "true");
-  		options.setCapability("browserConnectionEnabled", "true");
-  		options.setCapability("databaseEnabled", "true");
-  		options.setCapability("networkConnectionEnabled", "true");*/
-  		driver = new ChromeDriver();
-		
-		
+
+		driver = new ChromeDriver(); 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		//driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
